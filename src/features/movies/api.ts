3cc419317f1d7235daea0ed11d104c6c8d6e53a0
Garu2807 <arutyunyangagik@gmail.com>
2024-04-27 03:08:27 +0300

    movies

--- src/features/movies/api.ts
+++ src/features/movies/api.ts
@@ -1,7 +1,8 @@
 import { Movie } from './types/Movie';
 
-const apiKey = 'M4XR0MZ-20W4K5Q-NTSS050-QB7DC15';
-const apiUrl = 'https://api.kinopoisk.dev/v1.4/movie?page=1&limit=4&lists=top250';
+const apiKey = '';
+const apiUrl =
+  'https://api.kinopoisk.dev/v1.4/movie?page=1&limit=4&lists=top250';
 export const getMovies = async (): Promise<Movie[]> => {
   const response = await fetch(apiUrl, {
     headers: {
