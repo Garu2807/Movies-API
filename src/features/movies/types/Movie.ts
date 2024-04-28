508591f259ab48015d88e6c3fea59ef9d26d5b6e
Garu2807 <arutyunyangagik@gmail.com>
2024-04-28 05:42:03 +0300

    some style

--- src/features/movies/types/Movie.ts
+++ src/features/movies/types/Movie.ts
@@ -5,6 +5,13 @@ export type movie_img = {
 export type movie_raiting = {
   imdb: string;
 };
+export type Genre = {
+  name: string;
+};
+export type movie_back = {
+  url: string;
+  previewUrl: string;
+};
 export type Movie = {
   id: number;
   name: string;
@@ -12,5 +19,11 @@ export type Movie = {
   poster: movie_img;
   year: number;
   rating: movie_raiting;
+  alternativeName: string;
+  description: string;
+  ageRating: number;
+  genres: Genre[];
+  movieLength: number;
+  back: movie_back;
 };
 export type MovieId = Movie['id'];
